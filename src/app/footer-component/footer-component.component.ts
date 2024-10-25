899dee0817dbb4f0fd6868fa2236aa25de900df4
mootezbourguiba <mootezbourguiba73@gmail.com>
2024-10-25 18:32:54 +0100

    projet nodejs

---
+++ src/app/footer-component/footer-component.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-component',
+  templateUrl: './footer-component.component.html',
+  styleUrls: ['./footer-component.component.css']
+})
+export class FooterComponentComponent { }
